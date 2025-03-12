5c9edaf70fb805d1c60ffcf83ca2f5e8620570b7
Nwachukwu Clinton <nwachukwuclinton2@gmail.com>
2025-03-12 08:10:42 +0100

    seventh commit

--- meetgreetform/settings.py
+++ meetgreetform/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -122,8 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
